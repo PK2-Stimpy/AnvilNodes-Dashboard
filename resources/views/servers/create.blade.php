6146ec9cfc3af93d413b751cc0524fe1a1d7c636
PK2_Stimpy <31771533+PK2-Stimpy@users.noreply.github.com>
2022-06-01 23:36:23 +0200

    Maybe this works

--- resources/views/servers/create.blade.php
+++ resources/views/servers/create.blade.php
@@ -218,10 +218,10 @@
                                         </div>
                                     </div>
                                     <button type="submit" x-model="selectedProduct" name="product"
-                                        :disabled="product.minimum_credits > user.credits - user.intCreditUsage"
-                                        :class="product.minimum_credits > user.credits - user.intCreditUsage ? 'disabled' : ''"
+                                        :disabled="product.minimum_credits > (user.credits - user.intCreditUsage)"
+                                        :class="product.minimum_credits > (user.credits - user.intCreditUsage) ? 'disabled' : ''"
                                         class="btn btn-primary btn-block mt-2" @click="setProduct(product.id)"
-                                        x-text=" product.minimum_credits > user.credits - user.intCreditUsage ? '{{ __('Not enough') }} {{ CREDITS_DISPLAY_NAME }}!' : '{{ __('Create server') }}'">
+                                        x-text=" product.minimum_credits > (user.credits - user.intCreditUsage) ? '{{ __('Not enough') }} {{ CREDITS_DISPLAY_NAME }}!' : '{{ __('Create server') }}'">
                                     </button>
                                 </div>
                             </div>
